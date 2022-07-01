8553b0b633f021bf3c4114428b1018206b677069
Bradley-Coy <bradleycoy95@gmail.com>
2022-07-01 11:24:46 -0700

    first commit

---
+++ Variables/Program.cs
@@ -0,0 +1,11 @@
+﻿string myString = "fantastic";
+int myInt = 8;
+char myChar = 'L';
+double myDoub = 0.5;
+decimal myDec = 13.5m;
+bool myBool = true;
+
+if (myInt >= 8)
+{
+    Console.WriteLine($"My favorite word is {myString}. My favorite letter is {myChar}. I was {myDoub} my current age {myDec} years ago.");
+}
